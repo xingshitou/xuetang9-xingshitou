95e5a5999e008c5ba0587fe54f8d1eb64260cdf8
Administrator <Administrator@192.168.1.104>
2017-07-14 22:58:45 +0800

    第二次修改

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,14 @@
 
+/**
+ * 用来显示Github基本用法
+ * @author 连云港——星石头
+ * @version 1.0
+ * @date
+ */
 public class HelloWorld {
-
+	public static void main(String[] args) {
+		System.out.println("王者农药伤不起!");
+		System.out.println("再一次!");
+	}
 }
+
